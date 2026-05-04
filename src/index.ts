f87e3efdea2e6c8001990a02289b0441b15a7f04
aquapi <aquaplmc@gmail.com>
2026-05-04 21:50:52 +0700

    a little better dx when artifacts don't need to be exported

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { evaluate as globalEvaluate } from './globals.ts';
 import { IS_JIT } from './env/index.ts';
 
 export type ArtifactsRecord = Record<any, Artifact<any>>;
-export type Artifacts<T extends ArtifactsRecord> = <K extends keyof T>(k: K) => T[K]['~'];
+export type ArtifactGroup<T extends ArtifactsRecord> = <K extends keyof T>(k: K) => T[K]['~'];
 
-export const artifacts: <T extends ArtifactsRecord>(o: T) => Artifacts<T> = IS_JIT
+export const groupArtifacts: <T extends ArtifactsRecord>(o: T) => ArtifactGroup<T> = IS_JIT
   ? (o) => (globalEvaluate(), (k) => __rtcpl_atf__[o[k]])
   : (o) => (k) => __rtcpl_atf__[o[k]];
+
+export const artifact = <T>(id: Artifact<T>): T => __rtcpl_atf__[id] as any;
